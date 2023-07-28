afa14b51671050de3a49144dc5ad4a703c84fcae
Chen-max-hui <17775437319@163.com>
2023-07-28 21:22:39 +0800

    first commit

--- mybatis/src/main/java/com/mysql/mapper/SchoolClassMapper.java
+++ mybatis/src/main/java/com/mysql/mapper/SchoolClassMapper.java
@@ -9,7 +9,6 @@ public interface SchoolClassMapper {
     List<SchoolClass> selectAll();
     int deleteById(Integer id);
     int updateInfo(@Param("id") Integer id, @Param("className") String className, @Param("classDesc") String classDesc);
-
-    SchoolClass selectById2(Integer id);
+   SchoolClass selectById2(Integer id);
     int insertDemo(SchoolClass schoolClass);
 }
---
+++ vue/src/main/java/Structure.java
@@ -0,0 +1,9 @@
+public class Structure {
+    public static void main(String[] args) {
+
+    }
+    public int reverse(int x) {
+        //获取每个数字的
+        return 0;
+    }
+}
